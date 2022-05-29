39a4ab5852721a71e48beb03b1fb8d18c9de8351
shahinshaikh07 <shahin.shaikh97@gmail.com>
2022-05-29 23:47:10 +0530

    7th Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import Alert from './Components/Alert';
+import React, { useState } from 'react';
 
 function App() {
+const [alert, setAlert] = useState(null);
+const [mode, setMode] = useState('light');
+
+const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+ 
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (<>
-  <Navbar title="WriteOn"/>
-  <Textform heading="Enter text here"/>
+  <Navbar  title="WriteOn" mode={mode} toggleMode={toggleMode} key={new Date()}/>
+  <Alert alert={alert}/>
+  <Textform showAlert={showAlert}  heading="Enter text here" mode= {mode}/>
+
   </>
   );
 }
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -2,30 +2,38 @@ import React, {useState} from 'react'
 //import PropTypes from 'prop-types'
 
 export default function Textform(props) {
-const [text, setText] = useState("");
+    const [text, setText] = useState("");
     
 const handleLoCLick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to LowerCase!", "success");
 }
 
 const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText)
+     props.showAlert("Converted to UpperCase!", "success");
 }
 
 const handleClearClick = () => {
     let newText = "";
     setText(newText);
+     props.showAlert("Text Cleared!", "success");
 }
 
 const handleCopy = () => {
      navigator.clipboard.writeText(text); 
+      props.showAlert("Text, Copied!", "success");
 }
 
 const handleExtraSpace = () => {
     let newText = text.split(/[ ]+/);
-    setText(newText.join(" "));}
+    setText(newText.join(" "));
+props.showAlert("Extra spaces removed!", "success");
+}
+
+    
 
 
 const handleOnChange = (event) => {
@@ -34,21 +42,21 @@ const handleOnChange = (event) => {
  let wordCount = text === "" ? "0" : text.trim().split(/\s+/).length;
 
   return (
-   <div className='container'>
+   <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
        <h1 className='mb-4'>{props.heading}</h1>
        
 <div className="mb-3">
-    <textarea value={text} onChange={handleOnChange} placeholder='Enter your text here to analyze it' className="form-control" id="myBox" rows="8"></textarea>
+    <textarea value={text} onChange={handleOnChange}  style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'#042743'}} placeholder='Enter your text here to analyze it' className="form-control" id="myBox" rows="8"></textarea>
 </div>
 
 {/* Buttons providing different functionalities */}
 
-<button type="button" class="btn btn-primary mx-1 my-1" onClick={handleLoCLick}>Convert to Lowercase</button>
-<button type="button" class="btn btn-primary mx-1 my-1" onClick={handleUpClick} >Convert to Uppercase</button>
-<button type="button" class="btn btn-primary mx-1 my-1" onClick={handleClearClick} >Clear Text</button>
-<button type="button" class="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text </button>
-<button type="button" class="btn btn-primary mx-1 my-1" onClick={handleExtraSpace} >Remove Extra Spaces</button>
-<div className="container my-4">
+<button disabled={text.length===0} type="button" class="btn btn-primary mx-1 my-1" onClick={handleLoCLick}>Convert to Lowercase</button>
+<button disabled={text.length===0} type="button" class="btn btn-primary mx-1 my-1" onClick={handleUpClick} >Convert to Uppercase</button>
+<button disabled={text.length===0} type="button" class="btn btn-primary mx-1 my-1" onClick={handleClearClick} >Clear Text</button>
+<button disabled={text.length===0} type="button" class="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text </button>
+<button disabled={text.length===0} type="button" class="btn btn-primary mx-1 my-1" onClick={handleExtraSpace} >Remove Extra Spaces</button>
+<div className="container my-4"  style={{color: props.mode==='dark'?'white':'#042743'}}>
     <h3>Preview</h3>
     <p>
         {text.length > 0 ? text : "Type something above to preview it here"}
